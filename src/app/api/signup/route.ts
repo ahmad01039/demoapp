4d06e83fc37596debe66bca4e3df5cf369e154a1
Ahmad Raza <ahmad.raza@datics.ai>
2024-11-05 11:07:47 -0800

    setting up orm module for next js

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,20 +1,25 @@
 import bcrypt from "bcrypt";
 import client from "../../../lib/dbClient";
 
-
 export async function POST(req: Request) {
-  
   const { email, password, name } = await req.json();
 
   if (!email || !password || !name) {
-    return new Response(JSON.stringify({ error: "Missing fields" }), { status: 400 });
+    return new Response(JSON.stringify({ error: "Missing fields" }), {
+      status: 400,
+    });
   }
 
   try {
-    const existingUser = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+    const existingUser = await client.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
 
     if (existingUser.rows.length > 0) {
-      return new Response(JSON.stringify({ error: "User already exists" }), { status: 400 });
+      return new Response(JSON.stringify({ error: "User already exists" }), {
+        status: 400,
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -25,23 +30,14 @@ export async function POST(req: Request) {
 
     const user = result.rows[0];
 
-    
-      return new Response('Hello, Next.js!', {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-      });
-    
-    // return new Response(JSON.stringify({ message: "User created successfully", user }), { status: 201 });
+    return new Response(
+      JSON.stringify({ message: "User created successfully", user }),
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error creating user:", error);
-    return new Response(JSON.stringify({ error: "Internal server error" }), { status: 500 });
+    return new Response(JSON.stringify({ error: "Internal server error" }), {
+      status: 500,
+    });
   }
 }
-
-
-
-
